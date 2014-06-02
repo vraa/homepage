f40d89d2e9ea7a679ea1b746d18afbcc39769888
Veera <veera.k.sundar@gmail.com>
2014-06-01 20:46:00 -0500

    A basic skeleton with initial content.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,31 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-	  connect: {
-	    server : {
-	    	options : {
-	    		port : 9005,
-	    		keepalive : true
-	    	}
-	    },
-	    keepalive : true
-	  }
+		sass : {
+			dist : {
+				files : {
+					'css/main.css' : 'css/main.scss'
+				}
+			}
+		},
+		watch : {
+			css : {
+				files : '**/*.scss',
+				tasks : ['sass']
+			}
+		},
+		connect: {
+			server : {
+				options : {
+					port : 9005,
+					keepalive : true
+				}
+			},
+			keepalive : true
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 };
